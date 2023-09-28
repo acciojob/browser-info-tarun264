584a5b53018d98b3899c153278a588dd52f1fa98
Tarun Rana <66361151+tarun264@users.noreply.github.com>
2023-09-28 14:48:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+
+let browser=document.getElementById("browser-info");
+
+browser.innerHTML=  "You are using " + navigator.appName + " version " + navigator.appVersion;
